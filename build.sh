52965bfac4f15197772a174089e45c0c2bb91709
Bálint Dózsa <balint.dozsa@devertix.com>
2026-04-08 14:41:55 +0200

    Add 8.3

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-for v in "8.1" "8.2" "8.4";do
+for v in "8.1" "8.2" "8.3" "8.4";do
     cd $v && docker buildx build --platform=linux/amd64 -t ghcr.io/devertix/phpenv-mssql-oracle:$v . && cd ..
 done
